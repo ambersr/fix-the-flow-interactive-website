5c12e7fcebde314f2cad3de95bad656545996a64
ambersr <amber.schalker@hva.nl>
2024-12-05 13:00:22 +0100

    Variabelen aangemaakt javascript carousel

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+// Carousel reviewkaartjes
+
+const slides = document.querySelectorAll('.slide'); // link de aangemaakte variabele slides aan de class .slide
+const dots = document.querySelectorAll('.dot');  // link de aangemaakte variabele dots aan de class dot
+let currentslide = 0; // De variabele currentslide geeft aan dat de slide die op actief staat op 0 (1) begint.
+
