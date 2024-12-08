9e06862a563e354187c9400cc41d195c6751f5af
ambersr <amber.schalker@hva.nl>
2024-12-08 16:08:14 +0100

    menu gefixed

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,14 @@
+// Code voor openklappen hamburger menu in nav
+const menuButton = document.querySelector("#menuButton"); // Selecteert de knop met het id 'menuButton'
+
+const hetMenu = document.querySelector("#menu"); // Selecteert het menu-element met het id 'menu'
+
+menuButton.addEventListener("click", toggleMenu); // Als je het menu openklikt dan roept hij 'toggleMenu' aan
+
+function toggleMenu() { // Voegt of verwijdert de class 'open' van het menu-element 'hetMenu'
+    hetMenu.classList.toggle("open"); // Dit zorgt ervoor dat het menu zichtbaar wordt of verbergt bij elke klik
+}
+
 // Carousel reviewkaartjes
 
 const slides = document.querySelectorAll('.slide'); // link de aangemaakte variabele slides aan de class .slide
